4bb955b6f70e872ba3186a8b740724c591d11591
x1 <aaa@aaa.com>
2018-02-25 18:23:16 +0800

    feat: 增加了一个用于检测会话有效性的中间件

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 require('./globals');
 require('./setup-qcloud-sdk');
+require('./utils/test');    //便于实验室test.js的程序能在nodejs不手动重启的情况下验证。
+
+let ut = require('./utils/utils.js');
+let log = ut.logger(__filename);
 
 let express = require('express');
 let path = require('path');
@@ -25,6 +29,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//console.log('ccccc',process.env);
+
+//session校验中间件，避免后端程序中每行都要增加此项内容
+app.use(function (req, res, next) {
+    try {
+
+        ut.reqLog(req, res, next);
+
+        //如果是登录请求，则继续执行后续逻辑
+        if (req.originalUrl.indexOf('/login2') === 0) {
+            log.debug('SESSION:收到登录请求，不进行session校验。');
+            next();
+        }
+        //如果不是登录请求，则校验会话，若会话过期，则直接应答
+        else if (!ut.checkSession(req.get('session3rdKey'))) {
+            log.debug('SESSION:非登录请求，会话已超时，应答中将head.RTCD设置为超时。');
+            res.set('RTCD', 'RTCD_SESSION_TIMEOUT');
+            res.send('');
+        }
+        //如果会话未过期，则继续执行后续逻辑
+        else {
+            log.info('SESSION:会话未过期，放行。');
+            next();
+        }
+
+    }
+    catch (e) {
+        log.error(e);
+    }
+
+});
 
 //uploads是url中的访问路径，public/upload则是服务器端的相对路径。
 //以这种模式访问：http://localhost/uploads/avatar-3f91e492f204743d15259da44f16e903.jpg
