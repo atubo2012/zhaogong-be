558c6c03bb8230e5e0f9536341cedfa1a204b268
x1 <aaa@aaa.com>
2018-02-25 18:09:35 +0800

    feat: 增加getRandom，获取随机数通用函数 feat: 增加httpxReq，便于调用请求 style: 规范日志格式 refactor: 引入log4js日志

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,175 @@
 'use strict';
+/**
+ * 功能：
+ * 本文件作为所有常用utility类函数的主文件。
+ * 小程序端用的utils函数，应该直接复制于本文件中的函数。
+ *
+ * 场景：
+ * 本文件中的函数，被业务模块中使用，并在不同的应用中复用。
+ *
+ * 规范：
+ * 1、函数定义
+ * 2、函数导出：exports
+ * 3、函数的测试程序
+ * 4、注释风格：
+ *  (1)函数之间用/**分割，并两行
+ *  (2)测试程序与函数定义之间用//分割
+ *
+ */
+
+
+/**
+ * 本文件中使用的全局性参数
+ */
+let cf = require('../beconfig.js');
+let globalData = require('../globalData.js');
+
+
+/**
+ * 日志句柄。后端程序可以通过let log = ut.logger(__filename)的方式获得日志句柄。
+ * 默认的日志级别为debug，投产后为info。
+ * utils中公用函数的日志，使用trace级别。
+ */
+let log4js = require('log4js');
+log4js.configure(cf.logConfig);
+let logger = function(name){
+    return log4js.getLogger(name);
+};
+exports.logger=function(name){
+    return logger(name);
+};
+let l = logger('utils');    //本程序中使用的log句柄，供本文件中的函数使用。
+//
+// l.debug('log测试');
+//
+
+
+
+
+/**
+ * 会话有效期检查函数。
+ * 当前端在每次向后端发起请求时，后端要校验session3rd是否超时，如未超时则更新ut属性，如超时则从globalData中删除该会话id对应的session记录
+ * 检查是否存在session状态，并更新session的时间
+ * @param session3rdKey 会话id，由后端程序生成、前端程序登录后保存在客户端本地。
+ */
+let checkSession = function(session3rdKey){
+
+    let ret = null;
+    let now  = new Date().getTime();
+    let s = globalData.session[session3rdKey];
+    l.trace('checkSession',globalData);
+
+    if(s){
+        let then = new Date(s.ut).getTime();
+        l.trace(session3rdKey+'对应的会话存在','now',now,'then',then,'时间差',(now-then)/1000+'秒');
+
+        if((now-then)/1000>=cf.SESSION_EXPIRED){
+            l.trace('因会话超时，将删除该session!');
+            delete globalData.session[session3rdKey];
+            //l.trace('删除后的session:',globalData);
+            ret = false;
+        }else{
+            l.trace('会话未超时，将刷新会话内的时间!');
+            globalData.session[session3rdKey].ut = new Date(now).toLocaleString();
+            //l.trace('更新后的session:',globalData);
+            ret = true;
+        }
+    }else{
+        ret = false;
+    }
+
+    //遍历所有的会话，清理超时会话
+    let sessions = globalData.session;
+    for (let key in sessions) {
+        let then = new Date(sessions[key].ut).getTime();
+        if ((now - then) / 1000 >= cf.SESSION_EXPIRED) {
+            //l.trace('因会话超时，将删除'+key+'对应的session!');
+            delete sessions[key];
+            //l.trace('删除后的globalData.session',globalData.session);
+        }
+    }
+    return ret;
+};
+exports.checkSession = function (session3rdKey) {
+    return checkSession(session3rdKey);
+};
+
+
+
+/**
+ * 生成随机数的，windows环境下，生成date.gettime的数字；linux下根据设备文件生成
+ * @param cb
+ */
+let getRandom = function (cb) {
+    let random = null;
+    let cmd = null;
+    let exec = require('child_process').exec;
+    let os = require('os');
+
+    l.trace('生成随机数的OS类别:',os.type());
+
+    if(os.type()==='Linux'){
+        cmd = 'head -n1 /dev/urandom|md5sum|head -c16';
+        exec(cmd, function(error, stdout, stderr) {
+            random =stdout;
+            cb(random);
+        });
+    }else{
+        random = new Date().getTime().toString();
+        cb(random);
+    }
+};
+exports.getRandom = function (cb) {
+    return getRandom(cb);
+};
+// getRandom(function(random){
+//     l.info('生成随机数',random);
+// });
+
+
+/**
+ * 以http或https方式发起请求，应答结果由回调函数处理
+ * @param httpType http或https
+ * @param url 请求链接，https:// 之后的内容
+ * @param cb 回调函数
+ */
+let httpxReq = function (httpType,url,cb) {
+
+    let https = require(httpType);
+    let iconv = require("iconv-lite");
+
+    //l.trace('url:',url);
+    let _url=httpType+'://'+url;
+    //api.weixin.qq.com/sns/oauth2/access_token?appid="+cf.appId+"&secret="+cf.secret+"&code="+req.query.code+"&grant_type=authorization_code";
+
+
+    https.get(_url, function (res) {
+        let datas = [];
+        let size = 0;
+
+        res.on('data', function (data) {
+            datas.push(data);
+            size += data.length;
+            //process.stdout.write(data);
+        });
+        res.on("end", function () {
+            let buff = Buffer.concat(datas, size);
+            let result = iconv.decode(buff, "utf8");//转码//let result = buff.toString();//不需要转编码,直接tostring
+            //l.trace(result);
+            cb(result);
+
+        });
+    }).on("error", function (err) {
+        l.error(err,'hahah');
+    });
+};
+exports.httpReq = function (url,cb) {
+    return httpxReq('http', url,cb)
+};
+exports.httpsReq = function (url,cb) {
+    return httpxReq('https', url,cb)
+};
+
 
 let fmd = function(date, style) {
     let y = date.getFullYear();
@@ -15,8 +186,24 @@ let fmd = function(date, style) {
     return style.replace('yyyy', y).replace('MM', M).replace('dd', d).replace('hh', h).replace('mm', m).replace('ss', s);
 };
 
-//日志级别
-let logLevel = {
+
+let log = function(level,levelDesc,args) {
+    if (logLevel.LEVEL <= level) {
+        let now = fmd(new Date(), 'hhmmss');
+
+        //l.trace(now + '-['+levelDesc+']:========================================================');
+        for (let i = 0; i < args.length; i++) {
+            //若配置为前端使用的日志，则直接打印对象，便于查看。若配置为后端使用的日志，则将内容格式化后输出
+            if('front'===logLevel.TYPE){
+                l.trace(args[i]);
+            }else if('backend'===logLevel.TYPE){
+                l.trace(JSON.stringify(args[i],null,'\t'));
+            }
+        }
+        l.trace(now + '-['+levelDesc+']:==========================================================');
+    }
+};
+let logLevel = {    //日志界别
     FATAL:5,
     ERROR:4,
     WARN:3,
@@ -38,22 +225,7 @@ exports.debug = function () {
     log(logLevel.DEBUG,'debug',arguments);
 };
 
-let log = function(level,levelDesc,args) {
-    if (logLevel.LEVEL <= level) {
-        let now = fmd(new Date(), 'hhmmss');
 
-        console.log(now + '-['+levelDesc+']:========================================================');
-        for (let i = 0; i < args.length; i++) {
-            //若配置为前端使用的日志，则直接打印对象，便于查看。若配置为后端使用的日志，则将内容格式化后输出
-            if('front'===logLevel.TYPE){
-                console.log(args[i]);
-            }else if('backend'===logLevel.TYPE){
-                console.log(JSON.stringify(args[i],null,'\t'));
-            }
-        }
-        console.log(now + '-['+levelDesc+']:^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-    }
-};
 
 /**
  * 根据时间戳生成ID序号。适合并发量较小的应用使用。
@@ -63,11 +235,13 @@ let log = function(level,levelDesc,args) {
 exports.getId = function (idPrefix) {
     return getId(idPrefix)
 };
-
 let getId = function (idPrefix) {
     let ts = (new Date().getTime()).toFixed(0);
     return idPrefix + ts;
 };
+
+// let id = getId('A');
+// l.trace(id);
 
 
 /**
@@ -80,15 +254,15 @@ exports.reqLog = function (req, res, err) {
     return reqLog(req, res, err)
 };
 let reqLog = function (req, res, err) {
-    console.log(getCc('S'));
-    console.log('req.url:');
-    console.log(req.url);
-    console.log('req.query:');
-    console.log(req.query);
-    console.log(getCc('-'));
-    console.log('req.body:');
-    console.log(req.body);
-    console.log(getCc('E'));
+    l.trace(getCc('S'));
+    l.trace('req.url:');
+    l.trace(req.url);
+    l.trace('req.query:');
+    l.trace(req.query);
+    l.trace(getCc('-'));
+    l.trace('req.body:');
+    l.trace(req.body);
+    l.trace(getCc('E'));
 };
 
 /**
@@ -106,6 +280,8 @@ let getRf = function (str) {
     }
     return '[' + ret + ']';
 };
+// l.trace(getRf('abc'));
+
 
 /**
  * 在左方补齐空格
@@ -122,6 +298,8 @@ let getLf = function (str) {
     }
     return '[' + ret + ']';
 };
+// l.trace(getLf('abc'));
+
 
 /**
  * 在一行中打印一定长度的指定字符串。
@@ -137,6 +315,8 @@ let getCc = function (str) {
     }
     return ret;
 };
+// l.trace(getCc('-'));
+
 
 /**
  * 将对象格式化成字符
@@ -162,17 +342,75 @@ let prtObj = function (obj) {
 
     return str + '\n' + getCc('E');
 };
+// l.trace(prtObj({'a1':'b1','a2':'b2','a3':[{'c1':'c2'},{'d1':'d2'}]}));
 
 
 /**
- * 自测代码
+ * 手机号是否合法
+ * @param str
+ * @returns {boolean}
  */
-// let id = getId('A');
-// console.log(id);
-//
-//  console.log(getLf('abc'));
-//  console.log(getRf('abc'));
-// console.log(getCc('-'));
-//
-// console.log(prtObj({'a1':'b1','a2':'b2','a3':[{'c1':'c2'},{'d1':'d2'}]}));
+let isPoneAvailable = function(str) {
+    let myreg=/^[1][3,4,5,7,8][0-9]{9}$/;   //另一个校验规则：/^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/
+    return myreg.test(str)
+};
+exports.isPoneAvailable  = function (str){
+    return isPoneAvailable(str);
+};
+// l.info('手机号码测试',isPoneAvailable('13386222037'));
+
+
+
+/**
+ * 腾讯云短信发送服务
+ */
+
+/**
+ * 短信发送函数
+ * @param tpltId        短信模板ID，//短信模板的“ID”字段值，参考https://console.cloud.tencent.com/sms/smsContent/1400055205/0/10
+ * @param phoneNumbers  接收短信的号码（文本数组），如：["17701826978", "18616257890"]
+ * @param params        短信内容占位符（文本数组），如：["验证码002033", "有效期"]，params的元素个数，应与模板中的占位符数量相同，不能多也不能少。
+ */
+let sendSms = function(tpltId,phoneNumbers,params) {
+    let QcloudSms = require("qcloudsms_js");
+    let appid = cf.oaCfQQSms.appid;
+    let appkey = cf.oaCfQQSms.appkey;
+
+
+    let qcloudsms = QcloudSms(appid, appkey);
+
+    //用来演示的回调函数
+    function callback(err, res, resData) {
+        if (err)
+            l.error("短信发送错误: ", err);
+        else
+            l.trace("短信发送应答: ", resData);
+    }
+
+    //多条发送的demo
+    let msender = qcloudsms.SmsMultiSender();   //多条发送的对象
+    msender.sendWithParam("86", phoneNumbers, tpltId, params, "", "", "", callback);
+};
+exports.sendSms  = function (tpltId,phoneNumbers,params){
+    return sendSms(tpltId,phoneNumbers,params);
+};
+//sendSms(88752,['17701826978'],['1234','60']);
+
+
+
+let randomString = function(len) {
+    len = len || 32;    //这个默认复制的方式很帅
+    let chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+    let maxPos = chars.length;
+    let pwd = '';
+    for (let i = 0; i < len; i++) {
+        pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return pwd.toUpperCase();
+};
+exports.randomString  = function (len){
+    return randomString(len);
+};
+//console.log(randomString(4));
+
 
