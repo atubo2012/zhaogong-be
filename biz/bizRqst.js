59308f2a33655a7b3f33e859bc393991bf4d18ff
x1 <aaa@aaa.com>
2018-08-23 17:30:18 +0800

    docs: [] fix: [] refractor: [] chore: [] feat: Refine log for performance

--- biz/bizRqst.js
+++ biz/bizRqst.js
@@ -121,8 +121,9 @@ module.exports.list = function (req, res, err) {
 
             let coll = db.collection('rqst');
 
+
             coll.find(cond.query).sort({'updt': -1}).skip(cond.skip).limit(cond.limit).toArray(function (err, docs) {
-                console.info(err, docs);
+                console.info(err, docs.length);
                 res.send(JSON.stringify(docs)); //将后端将数据以JSON字符串方式返回，前端以query.data获取数据。
                 db.close();
             });
